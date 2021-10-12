8129a846758403069c914037e83fcc669608a1ce
team-hummelt <89801072+team-hummelt@users.noreply.github.com>
2021-10-12 12:04:47 +0200

    release 1.0.0 (#1)
    Co-authored-by: Jens Wiecker <jens@harz-web.com>

--- functions.php
+++ functions.php
@@ -1,4 +1,16 @@
 <?php
+// TODO SHOW SIDEBAR
+const HUPA_SIDEBAR = true;
+// TODO SHOW TOOLS
+const HUPA_TOOLS = true;
+// TODO SHOW CAROUSEL
+const HUPA_CAROUSEL = true;
+// TODO SHOW MAPS
+const HUPA_MAPS = true;
+// TODO SHOW CUSTOM FOOTER
+const CUSTOM_FOOTER = true;
+// TODO SHOW CUSTOM HEADER
+const CUSTOM_HEADER = true;
 
 	// style and scripts
 	 add_action( 'wp_enqueue_scripts', 'bootscore_5_child_enqueue_styles' );
