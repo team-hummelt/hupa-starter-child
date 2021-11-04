8292a39451772c0410080a699826f0245fcea483
team-hummelt <89801072+team-hummelt@users.noreply.github.com>
2021-11-04 17:46:05 +0100

    release 1.0.1 (#2) (#3)
    Co-authored-by: Jens Wiecker <jens@harz-web.com>
    
    Co-authored-by: Jens Wiecker <jens@harz-web.com>

--- functions.php
+++ functions.php
@@ -1,25 +1,11 @@
 <?php
-// TODO SHOW SIDEBAR
-const HUPA_SIDEBAR = true;
-// TODO SHOW TOOLS
-const HUPA_TOOLS = true;
-// TODO SHOW CAROUSEL
-const HUPA_CAROUSEL = true;
-// TODO SHOW MAPS
-const HUPA_MAPS = true;
-// TODO SHOW CUSTOM FOOTER
-const CUSTOM_FOOTER = true;
-// TODO SHOW CUSTOM HEADER
-const CUSTOM_HEADER = true;
-
 	// style and scripts
 	 add_action( 'wp_enqueue_scripts', 'bootscore_5_child_enqueue_styles' );
 	 function bootscore_5_child_enqueue_styles() {
-         
          // style.css
-         wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
-         
+         wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+         //wp_enqueue_style( 'starter-theme-custom-style', get_stylesheet_directory_uri() . '/css/lib/theme-custom.css' );
          // custom.js
          wp_enqueue_script('custom-js', get_stylesheet_directory_uri() . '/js/custom.js', false, '', true);
-
+         //wp_enqueue_script('bootscore-script', get_stylesheet_directory_uri() . '/js/lib/theme.js', false, '', true);
      } 
